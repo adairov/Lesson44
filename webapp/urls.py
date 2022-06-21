0ba9096b05791f36c6db7cea9aa7e96ea1a6a143
adairov <aibek.dairov@gmail.com>
2022-06-21 14:30:37 +0600

    game menu added commit

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from webapp.views import index_view, results_view
+
 urlpatterns = [
-    path("", index_view)
+    path("", index_view),
+    path("results/", results_view)
 ]
